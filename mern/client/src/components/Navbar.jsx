57a07b808a3788d7433398c761e792e3addfde42
xiztnt <chadolosky@gmail.com>
2026-02-02 22:22:35 -0500

    added logo and icon

--- mern/client/src/components/Navbar.jsx
+++ mern/client/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import { NavLink } from "react-router-dom";
 
+import RocketLogo from "../../assets/images/rocketElevators/RocketLogo.png"; // <-- import your local logo
+
 export default function Navbar() {
   return (
     <div>
       <nav className="flex justify-between items-center mb-6">
+        {/* to="/" is where the logo is navigating on click */}
         <NavLink to="/">
-          <img alt="MongoDB logo" className="h-10 inline" src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"></img>
+          <img alt="MongoDB logo" className="h-16 inline" src={RocketLogo}></img>
         </NavLink>
 
         <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3" to="/create">
